a9f70c89afd965f09ab724142e044f36c6beb3e5
YudumComez <ycomez@gmail.com>
2014-05-14 11:03:50 +0300

    hub

---
+++ WCFSelfHosting/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFSelfHosting
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProductService" in both code and config file together.
+    public class ProductService : IProductService
+    {
+        NorthwindEntities db = new NorthwindEntities();
+
+        public List<ProductDTO> ProductsSelect()
+        {
+            List<Products> productsList = db.Products.ToList();
+            List<ProductDTO> urunListesi = new List<ProductDTO>();
+
+            foreach (Products item in productsList)
+            {
+                ProductDTO urun = new ProductDTO() 
+                {
+                    ProductID = item.ProductID,
+                    ProductName = item.ProductName
+                };
+
+                urunListesi.Add(urun);
+            }
+
+            return urunListesi;
+            }
+    }
+
+    [DataContract]
+    public class ProductDTO
+    {
+        [DataMember]
+        public string ProductName { get; set; }
+
+        [DataMember]
+        public int ProductID { get; set; }
+    }
+}
---
+++ WCFSelfHosting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFSelfHosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(ProductService));
+            host.Open();
+            Console.WriteLine("Servisin Durumu : " + host.State);
+            Console.WriteLine("Servisi durdurmak için Enter'a basınız");
+            Console.ReadLine();
+            host.Close();
+            Console.WriteLine("Servisin Durumu : " + host.State);
+            Console.ReadLine();
+        }
+    }
+}
